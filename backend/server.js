5d0f7cfdab6725e194d1e626e0e53c7a62488674
Naga Sathya Bharadwaja Dandibhotla <nag.bharadwaja@gmail.com>
2025-03-15 21:25:06 -0400

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,6 @@ app.use('/exercises', exercisesRouter);
 
 // Production environment
 if (process.env.NODE_ENV === "production") {
-    console.log("In PRD");
     app.use(express.static(path.join(__dirname, '/frontend/dist')))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
